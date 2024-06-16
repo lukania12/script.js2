fa76d1c67a302105ce431af3cab95a06952bdb5b
lukania12 <166845052+lukania12@users.noreply.github.com>
2024-06-16 12:25:01 +0400

    Add files via upload

---
+++ day-75/script.js
@@ -0,0 +1,26 @@
+// //1) შექმენით მასივი და შეიტანეთ 10 სხვადასხვა მონაცემთა ტიპის მნიშვნელობა, შემდეგ 5 მონაცემს  შეუცვალეთ   მნიშნელობა და შემდეგ საბოლოოდ დაორინტეთ მასივი,
+
+// //1)შექმენით მასივი და შეიტანეთ 10 სხვადასხვა მონაცემთა ტიპის მნიშვნელობა
+
+// let Product1 = 'bread'
+// // let Product2 =  'cheese'
+// // let Product3 =  'new wine'
+// // let Product4 =  'oil'
+// // let Product5 =  'vodka'
+// // let Product6 =  'candies'
+// // let Product7 =  'white chocolates'
+// // let Product8 =  'clothes water bottle'
+// // let Product9 =  ' water bottle'
+// // let Product10 =
+
+// //                          #1      #2         #3        #4      #5        #6                #7           #8           #9           #10
+// let products = new Array('bread', 'cheese', 'new wine', 'oil', 'vodka', 'candies', 'white chocolates', 'clothes', 'water bottle', 'phills')
+
+// //##################################
+
+// //2) შემდეგ 5 მონაცემს  შეუცვალეთ   მნიშნელობა და შემდეგ საბოლოოდ დაორინტეთ მასივი,
+// products[5] = 'Georgian wine' //  #1
+// products[2] = 'fish' //  #2
+// products[3] = 'oldest wine' //  #3
+// products[6] = 'xhinkali' //  #4
+// products[7] = 'black chocolate' //  #5
